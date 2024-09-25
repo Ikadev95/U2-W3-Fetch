e9080c32d66ad7e73800663ba55b623d7c41678f
Ikadev95 <fegughi@gmail.com>
2024-09-25 17:14:50 +0200

    commit2

--- script.js
+++ script.js
@@ -15,26 +15,51 @@ function getLibrary (){
         books.forEach(book => {
             let newCol = document.createElement('div');
             newCol.classList.add('col', 'col-12','col-md-6','col-lg-3');
-            let i = 0;
+  
             newCol.innerHTML = 
-            `<div class="card">
+            `<div class="card border-0 shadow">
                 <img src="${book.img}" class="card-img-top" alt="book picture">
-                <div class="card-body">
+                <div class="card-body d-flex flex-column justify-content-around">
                     <h5 class="card-title">${book.title}</h5>
                     <p class="card-text">${book.price + ' €'}</p>
-                    <a href="#" class="btn btn-primary">Scarta</a>
+                   <div>
+                        <button class="btn btn-primary mb-2 addCart">Compra</button>
+                        <button class="btn btn-secondary discard">Scarta</button>
+                   </div>
                 </div>
             </div>` ;    
             libRow.appendChild(newCol);   
-            const btns = document.querySelectorAll('.btn');
+
+            const btns = document.querySelectorAll('.discard');
+
             btns.forEach((btn) => {
-                const card = btn.parentNode.parentNode.parentNode;
-                console.log(card);
+
+                const card = btn.closest('.col');
+
                 btn.addEventListener('click', function() {
                     card.classList.add('d-none');
                 });
             });
         });
+            const addBtn = document.querySelectorAll('.addCart');
+            const list = document.querySelector('.list-group');
+            
+            for (let i = 0; i < addBtn.length; i++) {
+                const button = addBtn[i];
+            
+                button.addEventListener('click', function() {
+                    const card = button.closest('.col');
+                    const titleCard = card.querySelector('.card-title').textContent;
+
+                    const li = document.createElement('li');
+                    li.innerText = titleCard;
+                    li.classList.add('list-group-item'); 
+                    list.appendChild(li);
+                });
+            }
+            
+
+        
     })
     .catch((error) =>{
         console.log(error);
